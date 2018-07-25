c6f287edda1c4f7244bc1af31f49ff0c34774bf8
Stephen Rushe <srushe@users.noreply.github.com>
2018-07-25 22:57:05 +0100

    Fix identification of phot from micropub.rocks example

--- lib/indieweb/post_types/identifier/photo.rb
+++ lib/indieweb/post_types/identifier/photo.rb
@@ -9,9 +9,14 @@ module Indieweb
 
           def identifier_from(data)
             return unless data.key?('photo')
-            return unless valid_url?(data['photo'])
+            return unless valid_url?(url_from(data['photo']))
 
             'photo'
+          end
+
+          def url_from(data)
+            photo = data.is_a?(Array) ? data[0] : data
+            photo.is_a?(String) ? photo : photo['value']
           end
         end
       end
